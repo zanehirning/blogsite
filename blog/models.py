ab98236722e7057b0e08275ea3a6e7d4a5944def
zanehirning <a02371568@usu.edu>
2023-01-20 01:51:58 -0700

    model setup

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
+class Blog(models.Model):
+    title = models.CharField(max_length=400)
+    author = models.CharField(max_length=255)
+    content = models.TextField()
+    posted = models.DateTimeField('date published')
+
+class Comments(models.Model):
+    blog = models.ForeignKey(Blog, related_name="comments", on_delete=models.CASCADE)
+    commenter = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    content = models.TextField()
+    posted = models.DateTimeField(auto_now_add=True)
+    def get_absolute_url(self):
+        return reverse('blog:index')
 # Create your models here.
