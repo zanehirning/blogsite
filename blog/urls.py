580251e116a9a66cfa11a5eb506b8613507874a9
zanehirning <a02371568@usu.edu>
2023-01-20 13:00:40 -0700

    more template work

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('entry/<int:pk>/', views.entry.as_view(), name='entry'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Blog, Comments
 
 # Create your views here.
 def home(request):
     return render(request, "blog/home.html")
+
+class entry(generic.DetailView):
+    model = Blog
+    template_name = "blog/entry.html"
+
+    def get_queryset(self):
+        return Blog.objects.order_by('-pub_date')
