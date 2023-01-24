12ad3fb09fb96e1fb2bad154287d3c65b1e9fef1
zanehirning <a02371568@usu.edu>
2023-01-23 22:35:18 -0700

    deployment prep

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0$gmn%g&#+ho6m74fz)$=cn&s#c0*3dm-j+o$$-(52_4harg()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blog\static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
